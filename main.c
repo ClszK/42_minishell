6cd94c05e5d61f3239726353dcd971c6e04b1954
Jehoon Lee <jeholee@cx2r1s1.42seoul.kr>
2024-01-05 05:23:59 +0900

    minishell: test main.c

--- main.c
+++ main.c
@@ -2,12 +2,51 @@
 
 int main(int argc, char **argv, char* envp[])
 {
-	// char *str;
-	(void)argc;
-	(void)argv;
-	// str = readline("> ");
-	// printf("%s", str);
+	char	*cmdline;
+	pid_t	child_pid;
+	t_envp	shell;
+	t_parse	cmd;
 
-	for (int i = 0; envp[i] ; i++)
-		printf("%s\n", envp[i]);
+	set_envp(envp, &shell);
+
+	while (1)
+	{
+		cmdline = readline("minishell$ ");
+		cmd.cmd_argv = ft_split(cmdline, ' ');
+		free(cmdline);
+    }
+	return (0);
 }
+
+    //  int 
+    //  main (int argc, char **argv)
+    //  {
+	// while (1){
+	// 	int childPid;
+	// 	char * cmdLine;
+
+	//         printPrompt();
+
+	//         cmdLine= readCommandLine(); //or GNU readline("");
+		
+	// 	cmd = parseCommand(cmdLine);
+
+	// 	record command in history list (GNU readline history ?)
+ 
+	// 	if ( isBuiltInCommand(cmd)){
+	// 	    executeBuiltInCommand(cmd);
+	// 	} else {		
+	// 	     childPid = fork();
+	// 	     if (childPid == 0){
+	// 		executeCommand(cmd); //calls execvp  
+			
+	// 	     } else {
+	// 		if (isBackgroundJob(cmd)){
+	// 		        record in list of background jobs
+	// 		} else {
+	// 			waitpid (childPid);
+
+	// 		}		
+	// 	    }
+	//         }
+    //  }
