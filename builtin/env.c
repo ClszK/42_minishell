4625e840a675ee0403eb26c4f3846b2d860a7447
Sumin Park <suminpar@cx2r1s1.42seoul.kr>
2024-01-08 09:21:54 +0900

    minishell: env print

---
+++ builtin/env.c
@@ -0,0 +1,20 @@
+#include "minishell.h"
+
+int	map_print(void *elem)
+{
+	t_map	*map;
+
+	map = (t_map *)elem;
+	errno = 0;
+	if (map->val)
+		if (printf("%s=%s\n", map->key, map->val) < 0)
+			return (errno);
+	return (0);
+}
+
+int	builtin_env(t_envp *env_c)
+{
+	if (dlst_print(env_c->head, map_print))
+		return (errno);
+	return (0);
+}
