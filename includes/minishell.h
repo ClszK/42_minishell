d28a29f2d4f8dc8b0ec9b2eb14c4acab4fcf285c
이제훈 <ljh@ijehun-ui-MacBookAir.local>
2024-01-10 08:49:03 +0900

    minishell: rearrange header and make parse.c

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,35 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ljh <ljh@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 08:10:11 by ljh               #+#    #+#             */
+/*   Updated: 2024/01/10 08:47:35 by ljh              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 # define EXIT_FAILURE 1
 
+# define SYNTAX_ERROR 1
+
 # include <stdio.h>
 # include <readline/readline.h>
-# include <readline/history.h> //readline add_history 등 
-# include <stdio.h> //printf  perror
-# include <stdlib.h> //malloc free exit getenv 
-# include <unistd.h> //write access read close fork getcwd chdir unlink execve dup dup2
-					 //pipe isatty ttyname ttyslot 
-# include <fcntl.h> //open 
-# include <sys/types.h> //pid_t
-# include <sys/wait.h> //wait
-# include <signal.h> //signal kill sigaction sigemptyset sigaddset 
-# include <sys/stat.h> //stat lstat fstat
-# include <dirent.h> //opendir readdir closedir
-# include <string.h> // strerror 
-# include <errno.h> // errno
-# include <sys/ioctl.h> //ioctl 
-# include <termios.h> //tcsetattr tcgetattr 
-# include <curses.h> 
-# include <term.h>	//tgetent tgetflag tgetnum tgetstr tgoto tputs
+# include <readline/history.h>		// readline add_history 등 
+# include <stdio.h> 				// printf  perror
+# include <stdlib.h> 				// malloc free exit getenv 
+# include <unistd.h> 				// write access read close fork getcwd 
+									// chdir unlink execve dup dup2
+									// pipe isatty ttyname ttyslot
+# include <fcntl.h>					// open 
+# include <sys/types.h>				// pid_t
+# include <sys/wait.h>				// wait
+# include <signal.h>				// signal kill sigaction sigemptyset 
+									// sigaddset 
+# include <sys/stat.h>				// stat lstat fstat
+# include <dirent.h>				// opendir readdir closedir
+# include <string.h>				// strerror 
+# include <errno.h>					// errno
+# include <sys/ioctl.h>				// ioctl 
+# include <termios.h>				// tcsetattr tcgetattr 
+# include <curses.h>
+# include <term.h>					// tgetent tgetflag tgetnum tgetstr
+									// tgoto tputs
 
 # include "double_lst.h"
 
-typedef struct s_cmd
+enum e_type
 {
-	char	**cmd_argv;
-	int		cmd_argc;
-}	t_cmd;
+	WORD,
+	REDIRECT,
+	PIPE,
+	BQUOTE,
+	SQUOTE
+};
+
+typedef struct s_parse
+{
+	char **cmd_argv;
+}	t_parse;
+
+typedef struct s_token
+{
+	enum e_type	type;
+	char		*token;
+}	t_token;
 
 typedef struct s_map
 {
@@ -43,22 +74,23 @@ struct s_lst
 	t_node	*tail;
 };
 
-typedef struct s_lst t_envp;
-typedef struct s_lst t_parse;
+typedef struct s_lst	t_envp;
+typedef struct s_lst	t_cmdline;
 
 /* utils.c*/
 long	ft_atol(char *str, int *flag);
 
 /* set.c */
 void	envp_init(char **envp, t_envp *env_c);
+void	cmdline_init(t_cmdline *cmdline);
 
 /* print.c */
 void	perror_exit(char *progname);
 
 /* builtin */
-int		builtin_echo(t_cmd *parse);
+int		builtin_echo(t_parse *parse);
 int		builtin_pwd(void);
 int		builtin_env(t_envp *env_c);
-int		builtin_exit(t_cmd *parse);
+int		builtin_exit(t_parse *parse);
 
 #endif
