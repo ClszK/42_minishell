3d9b3ff2cbf27f59f9705c647cc869f5a9efb528
Sumin Park <suminpark@Suminui-MacBookAir.local>
2024-01-10 08:51:03 +0900

    minishell: start export.c

---
+++ builtin/export.c
@@ -0,0 +1,34 @@
+#include "minishell.h"
+
+int	check_key(char *key)
+{
+	int	i;
+
+	if (ft_isalpha(key[0]) == 0 && key[0] != '_')
+		return (1);
+	i = 1;
+	while (key[i] && key[i] != '=')
+	{
+		if (ft_isalnum(key[i]) == 0 && key[i] != '_')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	builtin_export(t_parse *parse, t_envp *env_c)
+{
+	int	i;
+
+	if (parse->cmd_argv[1] == NULL)
+		return (builtin_env(env_c));
+	i = 1;
+	while (parse->cmd_argv[i])
+	{
+		if (check_key(parse->cmd_argv[i]))
+		{
+			printf("\n여기\n");
+		}
+		i++;
+	}
+}
