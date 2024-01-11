8a5c2695ee7a6119765307f371d0af7bc5463890
Clsz <45088680+ClszK@users.noreply.github.com>
2024-01-11 11:56:46 +0900

    minishell: delete main.c

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include "minishell.h"
 
+int	test_printf(void *param)
+{
+	t_token *token;
+
+	token = (t_token *)param;
+	printf("TYPE : %d%%\n", token->type);
+	printf("STR  : %s%%\n", token->str);
+	printf("\n");
+	return (0);
+}
+
+void	test_del(void *param)
+{
+	t_token	*token;
+
+	token = (t_token *)param;
+	free(token->str);
+}
+
 int main(int argc, char **argv, char **envp)
 {
 	char		*rline;
@@ -7,14 +26,17 @@ int main(int argc, char **argv, char **envp)
 	t_cmdline	cmdline;
 
 	envp_init(envp, &env_c);
-	cmdline_init(&cmdline);
 	while (1)
 	{
 		rline = readline("minishell$ ");
+		cmdline_init(&cmdline);
+		token_cmdline(rline, &cmdline);
+		dlst_print(&cmdline, test_printf);
+		dlst_del_all(&cmdline, test_del);
 		// cmd.cmd_argv = ft_split(cmdline, ' ');
 		// builtin_echo(&cmd);s
 		// builtin_pwd();
-		builtin_env(&env_c);
+		// builtin_env(&env_c);
 		// builtin_exit(&cmd);
 		free(rline);
     }
