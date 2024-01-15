641ba5d534a89734bfc501ab94b098221ea9f97c
Jehoon Lee <jeholee@cx2r1s1.42seoul.kr>
2024-01-16 06:27:39 +0900

    minishell: edit minsihell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ljh <ljh@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jeholee <jeholee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 08:10:11 by ljh               #+#    #+#             */
-/*   Updated: 2024/01/14 00:58:25 by ljh              ###   ########.fr       */
+/*   Updated: 2024/01/16 02:54:03 by jeholee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,26 @@ enum e_type
 	NONE,
 	WORD,
 	PIPE,
-	REDIRECT
+	OUTPUT,
+	APPEND,
+	INPUT,
+	HEREDOC
 };
-
-typedef struct s_parse
-{
-	char	*cmd_path;
-	char	**cmd_argv;
-}	t_parse;
 
 typedef struct s_token
 {
 	enum e_type	type;
 	char		*str;
 }	t_token;
+
+typedef struct s_parse
+{
+	int		cmd_argc;
+	char	*cmd_path;
+	char	**cmd_argv;
+	t_token	*stdin_token;
+	t_token	*stdout_token;
+}	t_parse;
 
 typedef struct s_map
 {
@@ -70,31 +76,34 @@ typedef struct s_map
 
 typedef struct s_lst	t_envp;
 typedef struct s_lst	t_cmdline;
-typedef struct s_lst	t_analyzs;
+typedef struct s_lst	t_analyze;
 
 /* utils.c*/
-long	ft_atol(char *str, int *flag);
+long		ft_atol(char *str, int *flag);
+enum e_type	token_type(char *str);
 
 /* set.c */
-void	envp_init(char **envp, t_envp *env_c);
-void	cmdline_init(t_cmdline *cmdline);
+void		envp_init(char **envp, t_envp *env_c);
+void		cmdline_init(t_cmdline *cmdline);
 
 /* print.c */
-void	perror_exit(char *progname);
+void		perror_exit(char *progname);
 
 /* parse.c */
-void	token_cmdline(char *rline, t_cmdline *cmdline);
+void		token_cmdline(char *rline, t_cmdline *cmdline);
 
 /* generate.c */
-t_token	*token_elem_generate(void);
+t_token		*token_elem_generate(void);
+t_parse		*parse_elem_generate(int cmd_argc);
 
 /* analyze.c*/
 
+
 /* builtin */
-int		builtin_echo(t_parse *parse);
-int		builtin_pwd(void);
-int		builtin_env(t_envp *env_c);
-int		builtin_exit(t_parse *parse);
-int		builtin_export(t_parse *parse, t_envp *env_c);
+int			builtin_echo(t_parse *parse);
+int			builtin_pwd(void);
+int			builtin_env(t_envp *env_c);
+int			builtin_exit(t_parse *parse);
+int			builtin_export(t_parse *parse, t_envp *env_c);
 
 #endif
