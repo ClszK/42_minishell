32e95bc671d327ffe5ea7a711e93757c2ab2694f
Jehoon Lee <jeholee@c5r1s1.42seoul.kr>
2024-01-17 18:20:51 +0900

    minishell: create free.c

--- srcs/utils/free.c
+++ srcs/utils/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ljh <ljh@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: jeholee <jeholee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:42:10 by ljh               #+#    #+#             */
-/*   Updated: 2024/01/17 13:06:43 by ljh              ###   ########.fr       */
+/*   Updated: 2024/01/17 15:56:38 by jeholee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,16 @@ void	token_elem_free(void *elem)
 	token = (t_token *)elem;
 	free(token->str);
 	free(elem);
+}
+
+void	parse_elem_free(void *elem)
+{
+	t_parse	*parse;
+	int		i;
+
+	parse = (t_parse *)elem;
+	i = -1;
+	while (parse->cmd_argv[++i])
+		free(parse->cmd_argv[i]);
+	free(parse->cmd_argv);
 }
